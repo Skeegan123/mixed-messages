f0a29fd5ef7381c0cfac00715febc301abe3fb81
Skeegan123 <mcfeegan123@gmail.com>
2021-10-31 23:45:27 -0600

    Finished!

--- createMessage.js
+++ createMessage.js
@@ -31,19 +31,31 @@ const adjectiveArray = () => {
 
 }
 
-console.log(nameArray());
-console.log(verbArray());
-console.log(adjectiveArray());
+// console.log(nameArray());
+// console.log(verbArray());
+// console.log(adjectiveArray());
 
 function createMessage(nameArray, verbArray, adjectiveArray) {
-    const randString = 0
+    const randString = Math.floor(Math.random() * 3);
     const randName = Math.floor(Math.random() * nameArray.length - 1);
+    const randName2 = Math.floor(Math.random() * nameArray.length - 1);
     const randVerb = Math.floor(Math.random() * verbArray.length - 1);
     const randAdjective = Math.floor(Math.random() * adjectiveArray.length - 1);
+    const randAdjective2 = Math.floor(Math.random() * adjectiveArray.length - 1);
+    const randAdjective3 = Math.floor(Math.random() * adjectiveArray.length - 1);
 
     switch(randString) {
         case 0: 
             console.log(`${nameArray[randName]} ${verbArray[randVerb]}ed the ${adjectiveArray[randAdjective]} dog.`);
+            break;
+
+        case 1:
+            console.log(`${nameArray[randName]} ${verbArray[randVerb]}ed with their boss ${nameArray[randName2]}.`);
+            break;
+
+        case 2:
+            console.log(`The ${adjectiveArray[randAdjective]} ${adjectiveArray[randAdjective2]} fox ${verbArray[randVerb]}s over the ${adjectiveArray[randAdjective3]} dog.`);
+            break;
     }
 }
 
